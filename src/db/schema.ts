8ddc688d9ba93cf65ff4d0d3c078cacaddba9b4e
Yash Hegde <yash4success@gmail.com>
2025-01-21 12:30:31 +0530

    db init

---
+++ src/db/schema.ts
@@ -0,0 +1,7 @@
+import { pgTable, serial, text, uuid, varchar } from "drizzle-orm/pg-core";
+
+export const UserTable = pgTable('users', {
+    id: uuid("id").primaryKey().defaultRandom(),
+    name: varchar("name", {length:255}).notNull(), 
+    email: varchar('email', { length: 255 }).notNull(),
+});
