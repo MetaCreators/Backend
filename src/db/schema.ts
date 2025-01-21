3b04e828e0c647ea001e145e4772dc62ec787b97
Yash Hegde <yash4success@gmail.com>
2025-01-21 22:18:56 +0530

    db schema WIP with references init

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,7 +1,31 @@
-import { pgTable, serial, text, uuid, varchar } from "drizzle-orm/pg-core";
+import { integer, pgTable, serial, text, uuid, varchar } from "drizzle-orm/pg-core";
 
 export const UserTable = pgTable('users', {
     id: uuid("id").primaryKey().defaultRandom(),
     name: varchar("name", {length:255}).notNull(), 
-    email: varchar('email', { length: 255 }).notNull(),
+    email: varchar('email', { length: 255 }).notNull().unique(),
+    availableCreds: integer("availableCreds").notNull().default(0),
+    trainingImgCloudUrl: varchar("trainingImgCloudUrl"),
+    generatedImgCloudUrl:varchar("generatedImgCloudUrl")
 });
+
+export const generatedImages = pgTable('generatedImages', {
+    id: uuid("id").primaryKey().defaultRandom(),
+    imageId:varchar("imageId").unique(),
+    modelId:varchar("modelId"),
+    userId:uuid("userId").references(()=>UserTable.id).notNull()
+})
+
+export const modelDetails = pgTable('modelDetails', {
+    id: uuid("id").primaryKey().defaultRandom(),
+    userId: uuid("userId"),
+    modelId:varchar("modelId").unique(),
+})
+
+export const userQueries = pgTable('userQueries', {
+    id: uuid("id").primaryKey().defaultRandom(),  
+    modelId: varchar("modelId"),
+    query: varchar("query"),
+    userId: uuid("userId"),
+    genImgId:varchar("imageId").unique()
+})
