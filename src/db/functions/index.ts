69b05fbc19702dbf0c11b944259980bf7191ec2b
Yash Hegde <yash4success@gmail.com>
2025-01-22 16:57:17 +0530

    almost done

--- src/db/functions/index.ts
+++ src/db/functions/index.ts
@@ -1,6 +1,6 @@
 import { eq } from "drizzle-orm";
 import { db } from "../db";
-import { trainingImages, UserTable } from "../schema";
+import { generatedImages, models, trainingImages, UserTable } from "../schema";
 import "dotenv/config"
 
 // create new user upon signup
@@ -26,51 +26,74 @@ async function getAllUsers() {
 //getAllUsers()
 
 
-// store user sent training images to cloud (get cloud url)
-async function createNewTrainingImage(userId:string) {
+// store user sent training images to cloud (get cloud url and then store it in db)
+async function storeUserTrainingImage(userId:string,cloudUrl:string,status:"pending" | "success" | "failed") {
     const response = await db.insert(trainingImages).values({
-        status: "pending",
+        status: status,//indicates whether saving to cloud was successful or not
         userId: userId,
+        cloudUrl:cloudUrl
     }).returning({
         id:trainingImages.id
     })
     console.log(response);
     return response
 }
-//createNewTrainingImage("c4396bd2-854b-4400-a7d9-72921c57e395")
+//storeUserTrainingImage("c4396bd2-854b-4400-a7d9-72921c57e395")
+async function getAllUserStoredImageData() {
+    const images = await db.select().from(trainingImages);
+    console.log(images);
+    return images;
+}
 
+//store user's new model on training complete
+async function createNewUserModel(userId:string,status:"pending" | "success" | "failed") {
+    // yaha model ayega
+     const response = await db.insert(models).values({
+        status: status,//indicates whether saving to cloud was successful or not
+        userId: userId,
+    }).returning({
+        id:models.id
+    })
+    console.log(response);
+    return response
+}
+createNewUserModel("c4396bd2-854b-4400-a7d9-72921c57e395", "success");
 //update training status upon completion
-async function updateTrainingStatus(status:"pending" | "success" | "failed",trainingId:string) {
-    const response = await db.update(trainingImages).set({
+async function updateModelTrainingStatus(status:"pending" | "success" | "failed",modelId:string) {
+    const response = await db.update(models).set({
         status:status
-    }).where(eq(trainingImages.id,trainingId)).returning({
-        id:trainingImages.id
+    }).where(eq(models.id,modelId)).returning({
+        id:models.id
     })
     console.log(response);
     return response
 }
 //updateTrainingStatus("success","dbce8b99-0cc7-4746-b140-1ea8576e5083")
 //get all training data
-async function getAllTrainingData() {
-    const trainings = await db.select().from(trainingImages);
-    console.log(trainings);
-    return trainings;
-}
-getAllTrainingData()
 
-//store user's new model on training complete
-async function createNewUserModel() {
-    
+async function getAllModelsData() {
+    const allModels = await db.select().from(models);
+    console.log(allModels);
+    return allModels;
 }
-
-// get user models
-async function getUserModels() {
-    
-}
+getAllModelsData()
 
 //store user's new generated Image
-async function storeGeneratedImage() {
-    
+async function storeGeneratedImage(cloudUrl:string,replicateUrl:string,modelId:string,replicateImageId:string,userId:string,prompt:string,status:"pending" | "success" | "failed",creditsUsed:number) {
+    const response = await db.insert(generatedImages).values({
+        status: status,//indicates whether saving to cloud was successful or not
+        userId: userId,
+        cloudUrl: cloudUrl,
+        replicateUrl,
+        modelId,
+        replicateImageId,
+        prompt,
+        creditsUsed
+    }).returning({
+        id:trainingImages.id
+    })
+    console.log(response);
+    return response
 }
 // get user images
 async function getUserImages() {
