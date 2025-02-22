fb19e9db5e07fd68e4eaf8d9b38a5e9bf3a41bcd
Yash Hegde <yash4success@gmail.com>
2025-02-22 20:48:38 +0530

    ..

--- src/routes/apiRoutes.ts
+++ src/routes/apiRoutes.ts
@@ -3,6 +3,7 @@ import { generateScript } from "../services/script";
 import { generateDescription } from "../services/description";
 import { authMiddleware } from "../middleware/auth";
 import { finetune } from "../services/thumbnail/finetune";
+import * as aws from "aws-sdk";
 
 import {
   GeminiService,
@@ -19,6 +20,13 @@ const geminiService = new GeminiService(process.env.GEMINI_API_KEY as string);
 const replicateService = new ReplicateService(
   process.env.REPLICATE_API_KEY as string
 );
+const bucket = "lithouseuserimages";
+const digiendpoint = new aws.Endpoint("blr1.digitaloceanspaces.com");
+const s3Client = new aws.S3({
+    endpoint: digiendpoint, 
+    accessKeyId: process.env.DIGIOCEAN_OBJECT_ACCESS_ID || "" ,
+    secretAccessKey: process.env.DIGIOCEAN_OBJECT_SECRET || "" 
+});
 
 // Initialize controller
 const imageController = new ImageController(geminiService, replicateService);
@@ -38,4 +46,34 @@ router.post(
   }
 );
 
+router.get(
+  "/get-presignedurl-upload",
+  async (req: Request, res: Response) => {
+    const { userId } = req.body;
+
+    const key = `${userId}/trainingImages/${Date.now()}.webp`;
+    const imageUrl="";
+    const response = await fetch(imageUrl); //what should this be ?
+    const arrayBuffer = await response.arrayBuffer();
+    const blob = Buffer.from(arrayBuffer);
+    const s3Params = {
+      Bucket: bucket,
+      Key: key,
+      ContentType: "image/webp"
+    };
+
+    const uploadUrl = await s3Client.getSignedUrlPromise("putObject", s3Params);
+    console.log("presigned URl for saving training images is ", uploadUrl);
+    const uploading = await fetch(uploadUrl, {
+      method: "PUT",
+      body: blob,
+      headers: {
+        "Content-Type": s3Params.ContentType
+      }
+    });        
+    console.log("Image saving to DO status", uploading);
+
+
+  }
+);
 export default router;
